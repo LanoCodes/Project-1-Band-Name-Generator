6861eff9989d0e9c49344c9380568728b1a630bd
LanoCodes <del.pow1@gmail.com>
2022-09-18 09:11:11 -0400

    First commit

---
+++ main.py
@@ -0,0 +1,7 @@
+print('Hello and welcome to my band name program :)')
+
+city = input('What city did you grow up in?\n')
+
+pet = input('You ever had a pet? What was it\'s name??\n')
+
+print('Here\'s your band name:\n' + city + ' ' + pet)
